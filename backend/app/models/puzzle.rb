bf8bf7898b34b4f12bc7a3639206156e44ddbd68
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-28 21:11:56 -0700

    added class methods to Puzzle

--- backend/app/models/puzzle.rb
+++ backend/app/models/puzzle.rb
@@ -3,4 +3,15 @@ class Puzzle < ActiveRecord::Base
     has_many :user_puzzles
     has_many :users, through: :user_puzzles
 
+    def self.available
+        # returns list of puzzles that are available to be checked out
+        self.all.select {|puzzle| puzzle.checked_out == false }
+    end
+
+    def self.unavailable
+        # returns list of puzzles that are NOT available to be checked out
+        self.all.select {|puzzle| puzzle.checked_out }
+    end
+
+
 end
