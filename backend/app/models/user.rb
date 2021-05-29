b66621f7bee730dfdb2edde734ff95567b71d94e
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-28 21:53:57 -0700

    user delete_user, contribute and renew methods

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
     def checkout(puzzle)
         self.user_puzzles.create(user: self, puzzle: puzzle, check_out_date: DateTime.now, due_date: DateTime.now)
-        # need to find out how to add 2 weeks to DateTime
+        # need to find out how to add 3 weeks to DateTime
         # for now check_out_date and due_date are the same
         puzzle.checked_out = true
         puzzle.save
@@ -16,6 +16,27 @@ class User < ActiveRecord::Base
         returned_puzzle.destroy
         puzzle.checked_out = false
         puzzle.save
+    end
+
+    def renew(puzzle)
+        renewed_puzzle = self.user_puzzles.find {|up| up.puzzle_id == puzzle.id}
+        # this will not work... need to determine how to add 3 weeks
+        # renewed_puzzle.due_date += 2 weeks
+        renewed_puzzle.save
+    end
+
+    def contribute(puzzle_name, pieces_missing, category)
+        # should I do validation of the inputs on this end or on the front end? need to reject inputs that are not the correct type. Seems like a controlled form in React would be a better option. 
+        Puzzle.create(name: puzzle_name, pieces_missing: pieces_missing, category: category, checked_out: false)
+        "Thank you for your contribution!"
+    end
+
+    def delete_user
+        if self.puzzles.length == 0
+            self.destroy
+        else
+            "You still has puzzles checked out. Please return all puzzles before deleting your account"
+        end
     end
     
 end
