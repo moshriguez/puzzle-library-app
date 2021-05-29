9bbc7ec0f9ecff195155135af9e6355826466289
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-28 21:35:17 -0700

    some comments and organization

--- backend/app/models/puzzle.rb
+++ backend/app/models/puzzle.rb
@@ -3,6 +3,8 @@ class Puzzle < ActiveRecord::Base
     has_many :user_puzzles
     has_many :users, through: :user_puzzles
 
+    #class methods
+
     def self.available
         # returns list of puzzles that are available to be checked out
         self.all.select {|puzzle| puzzle.checked_out == false }
@@ -12,6 +14,5 @@ class Puzzle < ActiveRecord::Base
         # returns list of puzzles that are NOT available to be checked out
         self.all.select {|puzzle| puzzle.checked_out }
     end
-
 
 end
