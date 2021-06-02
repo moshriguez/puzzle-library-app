8561c25d83b79e53e702aa6d0a4e40559143a88c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-02 15:57:49 -0700

    date formatting

--- frontend/src/components/UserPuzzleCard.js
+++ frontend/src/components/UserPuzzleCard.js
@@ -7,7 +7,7 @@ class UserPuzzleCard extends Component {
       <div className="card">
         <h2>{this.props.puzzle.name}</h2>
         <img src={this.props.puzzle.img_url} alt={this.props.puzzle.name} className="puzzle-img" />
-        <p>Due Date: {this.props.puzzle.due_date}</p>
+        <p>Due Date: {this.props.puzzle.due_date.slice(0, 10)}</p>
         <p>{this.props.puzzle.num_of_pieces} pieces</p>
         <p>{this.props.puzzle.pieces_missing} pieces missing </p>
         <button 
