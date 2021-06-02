097d9e21073729b87fc3e29fc11fee1abe63b493
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-02 11:32:00 -0700

    moved some methods to user_puzzle

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,30 +1,7 @@
 class User < ActiveRecord::Base
 
-    has_many :user_puzzles
+    has_many :user_puzzles, dependent: :destroy
     has_many :puzzles, through: :user_puzzles
-    
-    
-    def checkout(puzzle)
-        t = DateTime.now
-        self.user_puzzles.create(user: self, puzzle: puzzle, check_out_date: t, due_date: (t + 21))
-        puzzle.checked_out = true
-        puzzle.save
-    end
-    
-    def return(puzzle)
-        returned_puzzle = self.user_puzzles.find {|up| up.puzzle_id == puzzle.id}
-        returned_puzzle.destroy
-        puzzle.checked_out = false
-        puzzle.save
-    end
-    
-    def renew(puzzle)
-        # t = DateTime.now
-        renewed_puzzle = self.user_puzzles.find {|up| up.puzzle_id == puzzle.id}
-       
-        renewed_puzzle.due_date += (60 * 60 * 24 * 7 * 3)
-        renewed_puzzle.save
-    end
 
     def contribute(puzzle_name, pieces_missing, category)
         # should I do validation of the inputs on this end or on the front end? need to reject inputs that are not the correct type. Seems like a controlled form in React would be a better option. 
--- backend/app/models/user_puzzle.rb
+++ backend/app/models/user_puzzle.rb
@@ -3,4 +3,24 @@ class UserPuzzle < ActiveRecord::Base
     belongs_to :user
     belongs_to :puzzle
     
+    def checkout(puzzle, user)
+        t = DateTime.now
+        self.create(user: user, puzzle: puzzle, check_out_date: t, due_date: (t + 21))
+        puzzle.checked_out = true
+        puzzle.save
+    end
+    
+    def return(puzzle)
+        self.destroy
+        puzzle.checked_out = false
+        puzzle.save
+    end
+    
+    def renew(puzzle)
+        # t = DateTime.now
+        renewed_puzzle = self.find {|up| up.puzzle_id == puzzle.id}
+       
+        renewed_puzzle.due_date += (60 * 60 * 24 * 7 * 3)
+        renewed_puzzle.save
+    end
 end
