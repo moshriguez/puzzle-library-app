c3a60df9272eacee718c0cc0a7a39b4b0da9cb40
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-01 20:12:49 -0700

    started User components

---
+++ frontend/src/components/UserContainer.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+// import UserPuzzleCard from './UserPuzzleCard'
+
+const UserContainer = (props) => {
+	const [puzzles, setPuzzles] = useState([]);
+
+	useEffect(() => {
+		fetch(`http://localhost:9393/${props.userData.id}`)
+			.then((res) => res.json())
+			.then(puzzles => setPuzzles(puzzles.puzzles));
+	}, []);
+
+	return (
+		<div id="user-container">
+			<h1>{props.userData.name}</h1>
+			<h4>You have borrowed these puzzles:</h4>
+		</div>
+	);
+};
+
+export default UserContainer;
