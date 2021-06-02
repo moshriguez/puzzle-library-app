26847097d81e3e26ab0c36a50c07629bf0311420
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-01 22:10:29 -0700

    added pic and num of pieces

--- frontend/src/components/PuzzleCard.js
+++ frontend/src/components/PuzzleCard.js
@@ -6,8 +6,9 @@ class PuzzleCard extends Component {
     return (
       <div className="card">
         <h2>{this.props.puzzle.name}</h2>
-        {/* <img src={this.props.puzzle.image} alt={this.props.puzzle.name} className="puzzle-avatar" /> */}
-        <p>{this.props.puzzle.pieces_missing} pieces missing </p>
+        <img src={this.props.puzzle.img_url} alt={this.props.puzzle.name} className="puzzle-img" />
+        <p>{this.props.puzzle.num_of_pieces} pieces</p>
+        <p>{this.props.puzzle.pieces_missing} pieces missing</p>
         <button 
         disabled={this.props.puzzle.checked_out}
         className="borrow-btn" 
