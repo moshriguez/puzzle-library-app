9d9e63a24f8917a22f1ab2dc630e826ad21d8294
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-03 11:18:46 -0700

    added labels to inputs, made all inputs required, commented out category

--- frontend/src/components/Contribute.js
+++ frontend/src/components/Contribute.js
@@ -5,7 +5,7 @@ class Contribute extends React.Component {
 		name: '',
 		num_of_pieces: '',
         pieces_missing: '', 
-        category: '',
+        // category: '',
 		img_url: ''
 	}
 
@@ -19,12 +19,13 @@ class Contribute extends React.Component {
 		e.preventDefault();
 		const puzzleObj = {...this.state}
         puzzleObj.checked_out = false
+		puzzleObj.category = 'general'
 		this.props.handleContribute(puzzleObj)
 		this.setState({
 			name: '',
 			num_of_pieces: '',
 			pieces_missing: '', 
-			category: '',
+			// category: '',
 			img_url: ''
 		})
 	}
@@ -35,47 +36,57 @@ class Contribute extends React.Component {
 			<div id="contribute-form">
 				<form onSubmit={this.handleSubmit}>
 					<h3>Donate a Puzzle:</h3>
-                    <p>Thank you for donating a puzzle. Please fill out the form below.</p>
+                    <p>Thank you for donating a puzzle. Your contribution is appreciated!</p>
+					<p>Please fill out the form below.</p>
+					<label>Puzzle Name:</label>
 					<input
 						type="text"
 						name="name"
 						placeholder="Enter a puzzle name..."
+						required
 						className="input-text"
 						onChange={(e) => this.handleInupt(e)}
 						value={this.state.name}
 					/>
 					<br />
+					<label>Number of Pieces:</label>
                     <input
 						type="number"
 						name="num_of_pieces"
 						placeholder="How many pieces?"
+						required
 						className="input-text"
 						onChange={(e) => this.handleInupt(e)}
 						value={this.state.num_of_pieces}
 					/>
 					<br />
+					<label>Number of Missing Pieces:</label>
                     <input
 						type="number"
 						name="pieces_missing"
 						placeholder="Are there any pieces missing?"
+						required
 						className="input-text"
 						onChange={(e) => this.handleInupt(e)}
 						value={this.state.pieces_missing}
 					/>
 					<br />
-                    <input
+                    {/* <input
 						type="text"
 						name="category"
 						placeholder="Enter a category..."
+						required
 						className="input-text"
 						onChange={(e) => this.handleInupt(e)}
 						value={this.state.category}
 					/>
-					<br />
+					<br /> */}
+					<label>Image of Puzzle:<span>(Enter a URL for the puzzle image)</span></label>
 					<input
 						type="text"
 						name="img_url"
 						placeholder="Enter an image URL"
+						required
 						className="input-text"
 						onChange={(e) => this.handleInupt(e)}
 						value={this.state.img_url}
