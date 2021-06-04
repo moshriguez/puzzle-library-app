7ed6655dc9ad381628d0e2c64ce12db54afec580
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-04 12:32:47 -0700

    added filter for puzzles

--- frontend/src/components/PuzzleContainer.js
+++ frontend/src/components/PuzzleContainer.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PuzzleCard from './PuzzleCard';
 
 const PuzzleContainer = (props) => {
+	const [filterValue, setFilter] = useState('All');
+
+	const checkedOutFilter = () => {
+		return props.puzzleData.filter((puzzle) => {
+			switch (filterValue) {
+				case 'All':
+					return true;
+				case 'Available':
+					return puzzle.checked_out === false;
+			}
+		});
+	};
 	return (
 		<React.Fragment>
 			{props.noOneLoggedIn ? <p>Login to borrow a puzzle</p> : null}
+			<p>Filter by Availablity:</p>
+			<select id="filter" onChange={(e) => setFilter(e.target.value)}>
+				<option value="All">All</option>
+				<option value="Available">Available</option>
+			</select>
 			<div className="puzzle-collection">
-				{props.puzzleData.map((puzzle) => (
+				{checkedOutFilter().map((puzzle) => (
 					<PuzzleCard
 						key={puzzle.id}
 						puzzle={puzzle}
