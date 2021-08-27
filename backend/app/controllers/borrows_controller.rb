ef9597bee4b57b9f8b2dcd91d3aff76b41398a32
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-26 17:12:32 -0700

    borrow actions added to controller

--- backend/app/controllers/borrows_controller.rb
+++ backend/app/controllers/borrows_controller.rb
@@ -1,2 +1,46 @@
 class BorrowsController < ApplicationController
+
+    # check out a puzzle
+    def create 
+        borrow = Borrow.new(borrow_params)
+        t = Time.now
+        borrow.check_out_date = t
+        borrow.due_date = (t + 21.days)
+        if borrow.save
+            puzzle = Puzzle.find(borrow.puzzle_id)
+            puzzle.checked_out = true
+            puzzle.save
+            render json: {borrow: borrow, puzzle: puzzle}
+        else
+            render json: {error: borrow.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    # renew a puzzle
+    def update
+        borrow = Borrow.find_by(id: params[:id])
+        borrow.due_date += 3.weeks
+        if borrow.save
+            render json: {borrow: borrow}, status: :accepted
+        else
+            render json: {errors: borrow.errors.full_messages}, status: :unprocessable_entity
+        end
+
+    end
+
+    # return a puzzle
+    def destroy
+        borrow = Borrow.find_by(id: params[:id])
+        puzzle = Puzzle.find(borrow.puzzle_id)
+        puzzle.checked_out = false
+        puzzle.save
+        borrow.destroy
+        render json: {borrow: borrow}, status: :accepted
+    end
+
+
+    private
+    def borrow_params
+        params.permit(:user_id, :puzzle_id)
+    end
 end
