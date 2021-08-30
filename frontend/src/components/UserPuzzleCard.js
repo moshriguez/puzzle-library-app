a90323bd78a5af4d3937d006af490369e17963d3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-30 14:30:29 -0700

    transformed to functional component

--- frontend/src/components/UserPuzzleCard.js
+++ frontend/src/components/UserPuzzleCard.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserPuzzleCard extends Component {
+const UserPuzzleCard = ({ borrow, handleRenew, handleReturn }) => {
 
-  render() {
     return (
       <div className="card">
-        <h2>{this.props.borrow.name}</h2>
-        <img src={this.props.borrow.img_url} alt={this.props.borrow.name} className="puzzle-img" />
-        <p>Due Date: {this.props.borrow.due_date.slice(0, 10)}</p>
-        <p>{this.props.borrow.num_of_pieces} pieces</p>
-        <p>{this.props.borrow.pieces_missing} pieces missing </p>
+        <h2>{borrow.name}</h2>
+        <img src={borrow.img_url} alt={borrow.name} className="puzzle-img" />
+        <p>Due Date: {borrow.due_date.slice(0, 10)}</p>
+        <p>{borrow.num_of_pieces} pieces</p>
+        <p>{borrow.pieces_missing} pieces missing </p>
         <button 
         className="renew-btn" 
-        onClick={()=> this.props.handleRenew(this.props.borrow.id)}
+        onClick={()=> handleRenew(borrow.id)}
         >Renew</button>
         <button 
         className="return-btn" 
-        onClick={()=> this.props.handleReturn(this.props.borrow.id)}
+        onClick={()=> handleReturn(borrow.id)}
         >Return</button>
       </div>
     );
-  }
 
 }
 
