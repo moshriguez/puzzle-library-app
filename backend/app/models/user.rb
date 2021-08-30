208a6d265c929582e4aea3f8476baed6f8603405
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-30 14:15:45 -0700

    added validations on backend

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :borrows, dependent: :destroy
     has_many :puzzles, through: :borrows
 
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, length: {minimum: 6, maximum: 20}, if: -> {new_record? || !password.nil?}
+
 end
