6dc28ba294265f0bb4cec1f52b37b9f799c4ba41
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-07 16:25:43 -0700

    built each tab as separate components

--- frontend/src/components/UserTabs.js
+++ frontend/src/components/UserTabs.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
 
+import UserAccount from "./UserAccount";
+import UserHistory from "./UserHistory";
+import UserPuzzleContainer from "./UserPuzzleContainer";
+
 const UserTabs = () => {
     const [activeTab, setActiveTab] = useState('puzzles')
 
+    return (
+        <div className="tabs">
+            <ul className="user-tabs-nav">
+                <li>Puzzles</li>
+                <li>History</li>
+                <li>Account</li>
+            </ul>
+            <div className="tab-content">
+
+            </div>
+        </div>
+    )
 
 }
 
