b57ee756578ad26264de35b8c0153f42821ba2ff
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-11 23:01:51 -0700

    a change to the puzzle card

--- frontend/src/components/PuzzleCard.js
+++ frontend/src/components/PuzzleCard.js
@@ -4,17 +4,19 @@ const PuzzleCard = ({ puzzle, noOneLoggedIn, handleBorrow }) => {
 
   
   return (
-    <div className="card">
+    <div className="long-card">
       <h2>{puzzle.name}</h2>
       <img src={puzzle.img_url} alt={puzzle.name} className="puzzle-img" />
-      <p>{puzzle.num_of_pieces} pieces</p>
-      <p>{puzzle.pieces_missing} pieces missing</p>
-      <span>{puzzle.checked_out ? "checked out" : "available"}</span>
-      <button 
-        disabled={noOneLoggedIn || puzzle.checked_out}
-        className="btn" 
-        onClick={()=> handleBorrow(puzzle.id)}
-      >Borrow</button>
+      <div className="flex-container">
+        <p>{puzzle.num_of_pieces} pieces</p>
+        <span>({puzzle.pieces_missing} missing)</span>
+        <span>{puzzle.checked_out ? "checked out" : "available"}</span>
+        <button 
+          disabled={noOneLoggedIn || puzzle.checked_out}
+          className="btn" 
+          onClick={()=> handleBorrow(puzzle.id)}
+        >Borrow</button>
+      </div>
     </div>
   );
 
