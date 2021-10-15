6dd7dc79220df90d3e3133a72114db08705dee49
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-14 23:48:07 -0700

    adding confirmation message to delete user

--- frontend/src/components/UserAccount.js
+++ frontend/src/components/UserAccount.js
@@ -4,13 +4,28 @@ import ChangePassword from "./ChangePassword";
 
 const UserAccount = ({ deleteUser, errors, setErrors, setPopupMessage, userData }) => {
     const [formOpen, setFormOpen] = useState(false)
+    const [deleteConfirm, setDeleteConfirm] = useState(false)
 
     const handleClick = () => {
         setFormOpen(true)
     }
 
+    const handleDelete = () => {
+        setDeleteConfirm(true)
+    }
+
     return (
         <>
+            {deleteConfirm ? 
+                (
+                    <div className="modal-background">
+                        <div className="modal-content">
+                            <p>Are you sure you would like to delete your account?</p>
+                            <button onClick={() => deleteUser(userData)} >Yes</button>
+                            <button onClick={() => setDeleteConfirm(false)} >No</button>
+                        </div>
+                    </div>
+                ) : null}
             {formOpen ? 
                 <ChangePassword 
                 userId={userData.id}
@@ -23,7 +38,7 @@ const UserAccount = ({ deleteUser, errors, setErrors, setPopupMessage, userData 
                     <h1>{userData.username}</h1>
                     <button className="btn" onClick={handleClick}>Change Password</button>
                     <button 
-                        onClick={() => deleteUser(userData)}
+                        onClick={handleDelete}
                         className="delete-btn">
                         Delete Account
                     </button>
