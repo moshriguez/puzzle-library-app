59f3f5f099d9f232ec20bf85fb833f849a5e5389
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-19 23:04:48 -0700

    borrow button test

--- frontend/src/components/__tests__/PuzzleCard.test.js
+++ frontend/src/components/__tests__/PuzzleCard.test.js
@@ -5,5 +5,5 @@ test('if puzzle is checked out, "borrow" button is disabled', () => {
     const puzzle = {name: 'test', checked_out: true, num_of_pieces: 100, pieces_missing: 0 }
     render(<PuzzleCard type='puzzle' puzzle={puzzle}/>)
 
-    screen.debug()
+    expect(screen.getByRole('button', {name: /borrow/i})).toBeDisabled()
 })
