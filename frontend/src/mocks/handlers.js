83c46251506db43bc6c0203a423e53f1fb40fcb3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-21 23:03:42 -0700

    handlers added for user patch

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import { render } from "react-dom";
 
 const URL = 'http://localhost:3001/';
 
@@ -45,6 +46,37 @@ export const handlers = [
         return res(
             ctx.status(201),
             ctx.json({user: {id: 1, username: username, borrows: []}, jwt: 'testToken'})
+        )
+    }),
+    rest.patch(URL + 'users/1', (req, res, ctx) => {
+        const { password, newPassword } = req.body
+        if (password === 'wrongPassword') {
+            return res(
+                ctx.status(401),
+                ctx.json({error: 'Your password is not correct.'})
+            )
+        }
+        if (password === newPassword) {
+            return res(
+                ctx.status(400),
+                ctx.json({error: 'Your new password matches your old password. Are you sure you want to change your password?'})
+            )
+        }
+        if (newPassword.length > 20) {
+            return res(
+                ctx.status(406),
+                ctx.json({error: ['Password is too long (maximum is 20 characters)']})
+            )
+        }
+        if (newPassword.length < 6) {
+            return res(
+                ctx.status(406),
+                ctx.json({error: ['Password is too short (minimum is 6 characters)']})
+            )
+        }
+        return res(
+            ctx.status(202),
+            ctx.json({message: 'Your password was updated.'})
         )
     })
     
