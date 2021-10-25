148ab9591246d243b9be60b9d44802478d68dc51
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-24 17:28:40 -0700

    1 test working for App

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,20 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import App from './App';
 
 describe('App', () => {
-  test('loads all puzzles on render', () => {
+  test('loads all puzzles on render', async () => {
     render(<App />, {wrapper: Router});
     userEvent.click(screen.getByRole('link', {name: /puzzles/i}))
-    // screen.getByRole('')
+    const header = await screen.findByText(/Candyscape/i)
+    expect(header).toBeInTheDocument()
+    const header2 = await screen.findByText(/Sloths/i)
+    expect(header2).toBeInTheDocument()
+    // const borrowButtons = await screen.findAllByRole('button', {name: /borrow/i})
+    // userEvent.click(borrowButtons[0])
   })
-  test('if jwt is present in localStorage, user is automatically logged in', () => {
-    localStorage.setItem("jwt", 'testToken')
-    render(<App />, {wrapper: Router});
-    userEvent.click(screen.getByRole('link', {name: /user/i}))
-    screen.getByRole('')
-  })
+  // test('if jwt is present in localStorage, user is automatically logged in', async () => {
+  //   localStorage.setItem("jwt", 'testToken')
+  //   render(<App />, {wrapper: Router});
+  //   const userLink = await screen.findByRole('link', {name: /user/i})
+  //   userEvent.click(userLink)
+  //   screen.getByRole('')
+  // })
 
 })
